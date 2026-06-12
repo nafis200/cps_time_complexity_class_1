195cad6da0aba30a24d90c913b4a83f411748ba6
nafis ahamed <nafisahamed14@gmail.com>
2026-06-12 11:55:45 +0600

    sql

---
+++ SQL/Top_Travellers.sql
@@ -0,0 +1,34 @@
+
+CREATE TABLE Users (
+    id INT PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+CREATE TABLE Rides (
+    id INT PRIMARY KEY,
+    user_id INT,
+    distance INT,
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+);
+
+INSERT INTO Users (id, name) VALUES
+(1, 'Alice'),
+(2, 'Bob'),
+(3, 'Alex'),
+(4, 'Donald'),
+(7, 'Lee'),
+(13, 'Jonathan'),
+(19, 'Elvis');
+
+INSERT INTO Rides (id, user_id, distance) VALUES
+(1, 1, 120),
+(2, 2, 317),
+(3, 3, 222),
+(4, 7, 100),
+(5, 13, 312),
+(6, 19, 50),
+(7, 7, 120),
+(8, 19, 400),
+(9, 7, 230);
+
+SELECT u.name,COALESCE(r.distance, 0) as "travelled_distance" from Users as u LEFT JOIN (SELECT user_id, SUM(distance) as distance FROM Rides GROUP BY user_id) as r ON u.id = r.user_id ORDER BY "travelled_distance" DESC, u.name ASC  
